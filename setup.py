b22e4b60f7ead43c8d5ad2347873f2076e9efc81
Janne Vanhala <janne.vanhala@gmail.com>
2009-11-27 19:05:54 +0000

    Fixed #176

--- setup.py
+++ setup.py
@@ -13,15 +13,23 @@ setup(
     license = 'MIT',
     url = 'http://cloudsizzle.cs.hut.fi',
     description = 'Social study planner for Aalto University students',
-    install_requires = ['Django >= 1.1', 'Scrapy == 0.7'],
+    install_requires = [
+        'Django >= 1.1',
+        'Scrapy == 0.7',
+        'kpwrapper >= 0.9',
+    ],
     packages = find_packages(),
     test_suite = 'cloudsizzle.tests.suite',
-    tests_require = [
-        'bitten',
+    dependency_links = [
+        'http://public.futurice.com/~ekan/eggs',
+        'http://ftp.edgewall.com/pub/bitten/',
     ],
+    extras_require = {
+        'Bitten': ['bitten'],
+    },
     entry_points = {
         'distutils.commands': [
-            'unittest = bitten.util.testrunner:unittest'
+            'unittest = bitten.util.testrunner:unittest [Bitten]'
         ],
-    }
+    },
 )

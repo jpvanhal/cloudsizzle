2ef8a0bc3aea049347da9e63e28febce9f143d03
Kristoffer Snabb <kristoffer.snabb@tkk.fi>
2009-11-29 22:23:30 +0000

    added model for django, added template for django, made a djangokp which makes an initial query and adds the information now to a sqlite database, this should be changed later. some views and other django stuff.

--- cloudsizzle/webui/cloudsizzle/courselist/models.py
+++ cloudsizzle/webui/cloudsizzle/courselist/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Course(models.Model):
-    course_code = models.CharField(max_length=10)
+    course_code = models.CharField(max_length=11, primary_key=True)
     course_name = models.CharField(max_length=100)

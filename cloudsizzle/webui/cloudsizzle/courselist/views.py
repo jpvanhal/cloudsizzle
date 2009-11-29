e82c3a90ee27fb6fa37cc45989af56cef6512081
Kristoffer Snabb <kristoffer.snabb@tkk.fi>
2009-11-29 09:44:10 +0000

    courselist view which lists all the courses in rdf store, this takes about  minutes to run and the kp works as the model of django at the moment. This should be changed to use a SIB-->kp-->django database-->django view

--- cloudsizzle/webui/cloudsizzle/courselist/views.py
+++ cloudsizzle/webui/cloudsizzle/courselist/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 from kpwrapper import SIBConnection, Triple
 
 def index(request):
-    sc = SIBConnection('SIB console', 'preconfigured')      
-    results = sc.query(Triple(None, 'rdf:type', 'Course'))
-    ret = [triple.object for triple in results]
-    sc.close()
-    return HttpResponse("hello")
+    html = ""
+    with SIBConnection('SIB console', 'preconfigured') as sc:        
+        results = sc.query(Triple(None, 'rdf:type', 'Course'))
+        html = "<html><head><title>courselist</title></head><body><ul>"
+        for triple in results:
+            html += "<li>%s</li>" % triple.subject
+        html += "</ul></body></html>"
+    return HttpResponse(html)

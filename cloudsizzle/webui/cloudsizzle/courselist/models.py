f7998720a8bde77f4e3057c3f0db6e36801592c2
Janne Vanhala <janne.vanhala@gmail.com>
2009-11-30 15:38:47 +0000

    Removed old webui dir.

--- cloudsizzle/webui/cloudsizzle/courselist/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Course(models.Model):
-    course_code = models.CharField(max_length=11, primary_key=True)
-    course_name = models.CharField(max_length=100)

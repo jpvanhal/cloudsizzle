e370de2d73757ab3f943083ffc47238095daac57
Pang Bo <bpang@cc.hut.fi>
2009-12-03 21:14:56 +0000

    two parts of kp, UILoginKp is for django part to send username_password, ASILog inKP receives the query and authenticate password and give back the result.

--- cloudsizzle/asi/loginHandler.py
+++ cloudsizzle/asi/loginHandler.py
@@ -2,6 +2,13 @@ from kpwrapper import Triple
 import loginHandler
 import threading
 from asi import ASIConnection
+
+debug = True
+def debug_print(str):
+    if debug:
+        print('LoginHandler: %s' % str)
+
+
 class LoginHandler(threading.Thread):
 
     def __init__(self, kp, username_pw):
@@ -12,31 +19,29 @@ class LoginHandler(threading.Thread):
         self.username = str(username_pw).split(' ')[0]
         self.pw = str(username_pw).split(' ')[1]
      
-        print('password '+self.pw)
+        debug_print('password '+self.pw)
     def run(self):
-        conf = {
-        'base_url': 'http://cos.alpha.sizl.org',
-        'app_name': 'cloudsizzle02',
-        'app_password': 'aIR23eaiaJBR',		
-         }
-        conf.update({
+        conf = {}
+        import os
+        execfile(os.getenv('HOME', '.') + '/.asirc', conf)
+        conf['asi_app_params'].update({
         'username': self.username,
         'password': self.pw,
         })
 	
-        with ASIConnection(**conf) as ac:
-	
+        with ASIConnection(**conf['asi_app_params']) as ac:
             uid = ac.session['entry']['user_id']
-            print 'uid ', uid
+            debug_print( 'uid :'+ uid)
 
         if uid != 'null' or uid != None:
-            print self.username, " logged in"
-            self.kp.remove(Triple('users', 'updated', 'pang1 123456'))#remove(Triple('users', 'updated', self.username+" "+self.pw)) 
-            print self.username, 'authenticate', 'succeed'
+            debug_print( self.username + " logged in" )
+            self.kp.remove(Triple('users', 'login', self.username+" "+self.pw)) 
+            debug_print( self.username+ 'authenticate'+ 'succeed')
+            self.kp.remove(Triple(self.username, 'authenticate', 'succeed'))
             self.kp.insert(Triple(self.username, 'authenticate', 'succeed')) 
 				
         else:
-            print(self.username+" authenticate failed")
+            debug_print(self.username+" authenticate failed")
             self.kp.insert(Triple(self.username, 'authenticate', 'failed')) 
 
 

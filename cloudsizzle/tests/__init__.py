a3a7656e05badcc45db9dd1cbdaaab7bdea0ce23
Janne Vanhala <janne.vanhala@gmail.com>
2009-12-06 15:37:59 +0000

    Added a utility function for converting a list of triples into a graph.

--- cloudsizzle/tests/__init__.py
+++ cloudsizzle/tests/__init__.py
@@ -1,9 +1,12 @@
 import unittest
+import doctest
 from cloudsizzle.scrapers import tests as scrapers
+from cloudsizzle import utils
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(scrapers.suite())
+    suite.addTest(doctest.DocTestSuite(utils))
     return suite
 
 if __name__ == '__main__':
---
+++ cloudsizzle/utils.py
@@ -0,0 +1,20 @@
+def make_graph(triples):
+    """Transforms a list of triples into a graph.
+
+    >>> from kpwrapper import Triple
+    >>> triples = [Triple('T-76.4115', 'rdf:type', 'Course'),
+    ...     Triple('T-76.4115', 'name', 'Software Development Project'),
+    ...     Triple('T-76.4115', 'extent', '5-8')]
+    >>> make_graph(triples) #doctest: +NORMALIZE_WHITESPACE
+    {uri('T-76.4115'): {uri('rdf:type'): literal('Course'),
+                        uri('name'): literal('Software Development Project'),
+                        uri('extent'): literal('5-8')}}
+
+    """
+    graph = {}
+    for triple in triples:
+        s, p, o = triple.subject, triple.predicate, triple.object
+        if s not in graph:
+            graph[s] = {}
+        graph[s][p] = o
+    return graph

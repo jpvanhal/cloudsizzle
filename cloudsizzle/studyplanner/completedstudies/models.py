022d8b992a88fd4c489c068ba57b4b2fcf6dde98
Janne Vanhala <janne.vanhala@gmail.com>
2009-12-07 20:00:09 +0000

    Allow null values for ocr and cr fields of CompletedCourse

--- cloudsizzle/studyplanner/completedstudies/models.py
+++ cloudsizzle/studyplanner/completedstudies/models.py
@@ -8,15 +8,15 @@ class CompletedCourse(models.Model):
     """
     Model for completed studies
     """
-    student = models.ForeignKey(User, related_name='completed_courses') 
+    student = models.ForeignKey(User, related_name='completed_courses')
     code = models.CharField(max_length=11)
     name = models.CharField(max_length=100)
-    cr = models.IntegerField()
-    ocr = models.IntegerField()
+    cr = models.IntegerField(null=True)
+    ocr = models.IntegerField(null=True)
     grade = models.CharField(max_length=5)
     date = models.DateField()
     teacher = models.CharField(max_length=60)
-    
+
 class Teacher(models.Model):
     """
     should be updated for the teachers to combine them with course information

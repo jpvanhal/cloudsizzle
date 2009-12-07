48ea5c24fc3bc4335b95023d7964afba739d6981
Janne Vanhala <janne.vanhala@gmail.com>
2009-12-07 22:11:04 +0000

    Only query user's completed courses instead of all users' completed courses. Require that user is logged in.

--- cloudsizzle/studyplanner/completedstudies/views.py
+++ cloudsizzle/studyplanner/completedstudies/views.py
@@ -1,8 +1,10 @@
 # Create your views here.
 from studyplanner.completedstudies.models import CompletedCourse
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-    courses = CompletedCourse.objects.all()
-    return render_to_response('completed.html', {
+    courses = CompletedCourse.objects.filter(student=request.user)
+    return render_to_response('completed.html', {'user': request.user,
         'courses': courses})

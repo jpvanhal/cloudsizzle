fd9e25da2836ca860fc71bd2d0ed8b9fe80fc6b9
Janne Vanhala <janne.vanhala@gmail.com>
2009-12-07 13:55:55 +0000

    Removed old files

--- cloudsizzle/studyplanner/frontpage/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-
-# Create your views here.
-def frontpage(request):
-    login = request.session.get('login', "")
-    
-    return render_to_response('frontpage.html', {
-        'login': login
-    })

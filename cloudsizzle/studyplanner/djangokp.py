fa125570f424309910676ed69cbfc0c1051118d8
Janne Vanhala <janne.vanhala@gmail.com>
2009-12-07 22:08:27 +0000

    Revert previous changes.

--- cloudsizzle/studyplanner/djangokp.py
+++ cloudsizzle/studyplanner/djangokp.py
@@ -19,32 +19,32 @@ department_map = {}
 
 #query the course codes
 with SIBConnection('SIB console', 'preconfigured') as sc:
-##    faculties = sc.query(Triple(None, 'rdf:type', 'Faculty'))
-##    for triple in faculties:
-##        faculty = Faculty(code=triple.subject)
-##        populate_object(faculty, triple.subject, ['name'])
-##        faculty.slug = faculty.code.lower()
-##        faculty.save()
-##        faculty_map[str(triple.subject)] = faculty
-##
-##    departments = sc.query(Triple(None, 'rdf:type', 'Department'))
-##    for triple in departments:
-##        department = Department(code=triple.subject)
-##        populate_object(faculty, triple.subject, ['name'])
-##        faculty_triple = sc.query(Triple(triple.subject, 'faculty', None))[0]
-##        department.faculty = faculty_map[str(faculty_triple.object)]
-##        department.slug = department.code.lower()
-##        department.save()
-##        department_map[str(triple.subject)] = department
-##
-##    courses = sc.query(Triple(None, 'rdf:type', 'Course'))
-##    for triple in courses:
-##        course = Course(code=triple.subject)
-##        populate_object(faculty, triple.subject, ['name', 'extent', 'content', 'teaching_period', 'learning_outcomes', 'prerequisites', 'study_materials'])
-##        department_triple = sc.query(Triple(triple.subject, 'department', None))[0]
-##        course.slug = course.code.lower()
-##        course.department = department_map[str(department_triple.object)]
-##        course.save()
+    faculties = sc.query(Triple(None, 'rdf:type', 'Faculty'))
+    for triple in faculties:
+        faculty = Faculty(code=triple.subject)
+        populate_object(faculty, triple.subject, ['name'])
+        faculty.slug = faculty.code.lower()
+        faculty.save()
+        faculty_map[str(triple.subject)] = faculty
+
+    departments = sc.query(Triple(None, 'rdf:type', 'Department'))
+    for triple in departments:
+        department = Department(code=triple.subject)
+        populate_object(faculty, triple.subject, ['name'])
+        faculty_triple = sc.query(Triple(triple.subject, 'faculty', None))[0]
+        department.faculty = faculty_map[str(faculty_triple.object)]
+        department.slug = department.code.lower()
+        department.save()
+        department_map[str(triple.subject)] = department
+
+    courses = sc.query(Triple(None, 'rdf:type', 'Course'))
+    for triple in courses:
+        course = Course(code=triple.subject)
+        populate_object(faculty, triple.subject, ['name', 'extent', 'content', 'teaching_period', 'learning_outcomes', 'prerequisites', 'study_materials'])
+        department_triple = sc.query(Triple(triple.subject, 'department', None))[0]
+        course.slug = course.code.lower()
+        course.department = department_map[str(department_triple.object)]
+        course.save()
 
     completedcr = sc.query(Triple(None, 'has_completed', None))
     for triple in completedcr:

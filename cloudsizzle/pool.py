8b67bd3ea0ee6aba5bb1cb60084004a6c08aca7a
Janne Vanhala <janne.vanhala@gmail.com>
2010-01-26 11:30:35 +0000

    Refactored ConnectionPool to a general ObjectPool, which ConnectionPool inherits.

--- cloudsizzle/pool.py
+++ cloudsizzle/pool.py
@@ -2,14 +2,61 @@ from threading import Condition
 from kpwrapper import SIBConnection
 from contextlib import contextmanager
 
-class ConnectionPool(object):
-    """A cache for SIB connections so that the connections can be reused."""
+class ObjectPool(object):
+    """A cache for objects so that they can be reused."""
 
     def __init__(self):
         self._lock = Condition()
         self._unlocked = set()
         self._locked = set()
     
+    def _create(self):
+        raise NotImplemented
+    
+    def _expire(self, obj):
+        raise NotImplemented
+
+    def _validate(self, obj):
+        raise NotImplemented
+
+    def acquire(self):
+        """Acquire an object for use.
+        
+        If there are any objects available in the pool, removes one 
+        from the pool and returns it. Otherwise creates a new object 
+        and returns it.
+
+        Acquired object must be released back to the pool with the 
+        release() method.
+
+        """
+        with self._lock:
+            if self._unlocked:
+                while self._unlocked:
+                    obj = self._unlocked.pop()
+                    if self._validate(obj):
+                        self._locked.add(obj)
+                        return obj
+                    else:
+                        self._expire(obj)
+            obj = self._create()
+            self._locked.add(obj)
+            return obj
+    
+    def release(self, obj):
+        """Release a used object back to the pool for reuse.
+        
+        Arguments:
+        obj -- object to be released
+
+        """
+        with self._lock:
+            self._locked.remove(obj)
+            self._unlocked.add(obj)
+
+class ConnectionPool(ObjectPool):
+    """A cache for SIB connections so that they can be reused."""
+
     def _create(self):
         conn = SIBConnection(method='preconfigured')
         conn.open()
@@ -20,41 +67,6 @@ class ConnectionPool(object):
 
     def _validate(self, connection):
         return True
-
-    def acquire(self):
-        """Acquire a connection object for use.
-        
-        If there are any connections available in the pool, removes one 
-        from the pool and returns it. Otherwise creates a new connection 
-        and returns it.
-
-        Acquired connections must be released back to the pool with the 
-        release() method.
-
-        """
-        with self._lock:
-            if self._unlocked:
-                while self._unlocked:
-                    conn = self._unlocked.pop()
-                    if self._validate(conn):
-                        self._locked.add(conn)
-                        return conn
-                    else:
-                        self._expire(conn)
-            conn = self._create()
-            self._locked.add(conn)
-            return conn        
-    
-    def release(self, connection):
-        """Release a connection in use back to the pool for reuse.
-        
-        Arguments:
-        connection -- connection to be released
-
-        """
-        with self._lock:
-            self._locked.remove(connection)
-            self._unlocked.add(connection)
 
 _pool = ConnectionPool()
 

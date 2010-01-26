7fde50cdcb65220b8068296e58739c7ebac3f6a6
Janne Vanhala <janne.vanhala@gmail.com>
2010-01-26 10:15:52 +0000

    Added some docstrings.

--- cloudsizzle/pool.py
+++ cloudsizzle/pool.py
@@ -2,28 +2,16 @@ from threading import Condition
 from kpwrapper import SIBConnection
 from contextlib import contextmanager
 
-"""
->>> pool = ConnectionPool()
->>> sc = pool.acquire()
->>> instanceof(sc, SIBConnection)
-True
->>> sc2 = pool.acquire()
->>> id(sc) != id(sc2)
-True
->>> pool.release(sc2)
->>> sc3 = pool.acquire()
->>> id(sc2) == id(sc3)
-True
-
-"""
 class ConnectionPool(object):
+    """A cache for SIB connections so that the connections can be reused."""
+
     def __init__(self):
         self._lock = Condition()
         self._unlocked = set()
         self._locked = set()
     
     def _create(self):
-        conn = WrappedSIBConnection(method='preconfigured')
+        conn = SIBConnection(method='preconfigured')
         conn.open()
         return conn
     
@@ -34,6 +22,16 @@ class ConnectionPool(object):
         return True
 
     def acquire(self):
+        """Acquire a connection object for use.
+        
+        If there are any connections available in the pool, removes one 
+        from the pool and returns it. Otherwise creates a new connection 
+        and returns it.
+
+        Acquired connections must be released back to the pool with the 
+        release() method.
+
+        """
         with self._lock:
             if self._unlocked:
                 while self._unlocked:
@@ -48,6 +46,12 @@ class ConnectionPool(object):
             return conn        
     
     def release(self, connection):
+        """Release a connection in use back to the pool for reuse.
+        
+        Arguments:
+        connection -- connection to be released
+
+        """
         with self._lock:
             self._locked.remove(connection)
             self._unlocked.add(connection)

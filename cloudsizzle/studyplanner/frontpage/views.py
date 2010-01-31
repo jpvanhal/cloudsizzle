f17bf9e2c0b46c382e052c8a13cc4c7e35dd9ee0
Kristoffer Snabb <kristoffer.snabb@tkk.fi>
2010-01-31 10:07:20 +0000

    changed the forms so that they can be handled on the django side better

--- cloudsizzle/studyplanner/frontpage/views.py
+++ cloudsizzle/studyplanner/frontpage/views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def index(request):
     t = loader.get_template("frontpage/login-register.html")
-    c = Context({'loginform': LoginForm()})
+    c = Context({'loginform': AuthenticationForm(),
+                 'registerform': RegisterForm()})
     return HttpResponse(t.render(c))
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100, widget=forms.PasswordInput(render_value=False))
+class RegisterForm(UserCreationForm):
+    firstname = forms.CharField(label='First name', widget=forms.TextInput())
+    lastname = forms.CharField(label='Last name', widget=forms.TextInput())
+    email = forms.EmailField(label='Email', widget=forms.TextInput())
+
+
+def submit_registration(request):
     

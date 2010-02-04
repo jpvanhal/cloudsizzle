0cfc91bc888776cfde1ec0d62fdca58e15f91072
Janne Vanhala <janne.vanhala@gmail.com>
2010-02-04 12:01:15 +0000

    Added a function for fetching RDF graphs, see the docstring for an example. Modified make_graph to convert triple nodes to str

--- cloudsizzle/utils.py
+++ cloudsizzle/utils.py
@@ -1,19 +1,83 @@
+from cloudsizzle import pool
+from cloudsizzle.kp import Triple, uri, literal
+
+RDF_SCHEMA_URI = 'http://www.w3.org/2000/01/rdf-schema#'
+RDF_SYNTAX_NS_URI = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+
+def fetch_rdf_graph(subject):
+    """
+    Example usage:
+
+    >>> uri = 'http://cos.alpha.sizl.org/people/ID#dn3FNGIomr3OicaaWPEYjL'
+    >>> g = fetch_rdf_graph(uri)
+    >>> from pprint import pprint
+    >>> pprint(g)
+    {'address': 'None',
+     'avatar': {'link': {'href': '/people/dn3FNGIomr3OicaaWPEYjL/@avatar',
+                         'rel': 'self'},
+                'status': 'not_set'},
+     'birthdate': 'None',
+     'description': 'None',
+     'gender': 'None',
+     'irc_nick': 'None',
+     'is_association': 'None',
+     'msn_nick': 'None',
+     'name': {'family_name': 'Jannu15',
+              'given_name': 'Testi',
+              'unstructured': 'Testi Jannu15'},
+     'phone_number': 'None',
+     'role': 'None',
+     'status': {'changed': 'None', 'message': 'None'},
+     'updated_at': '2009-08-14T15:04:46Z',
+     'username': 'testijannu15',
+     'website': 'None'}
+
+    """
+    with pool.get_connection() as sc:
+        triplets = sc.query(Triple(subject, None, None))
+
+    graph = {}
+    for triplet in triplets:
+        # Skip triplets that define RDF ontology
+        if triplet.predicate.startswith(RDF_SYNTAX_NS_URI):
+            continue
+
+        s = str(triplet.subject)
+        p = str(triplet.predicate)
+        o = str(triplet.object)
+
+        if isinstance(triplet.predicate, uri):
+            p = p.split('#')[-1]
+
+        # The last condition is there to prevent wandering into RDF
+        # ontology definitions
+        if isinstance(triplet.object, uri) and not o.startswith(RDF_SCHEMA_URI):
+            value = fetch_rdf_graph(o)
+        else:
+            value = o
+        if s not in graph:
+            graph[s] = {}
+        if p not in graph[s]:
+            graph[s][p] = value
+        elif isinstance(graph[s][p], list):
+            graph[s][p].append(value)
+        else:
+            graph[s][p] = [graph[s][p], value]
+
+    return graph[subject]
+
 def make_graph(triples):
     """Transforms a list of triples into a graph.
 
     >>> from cloudsizzle.kp import Triple, uri, literal
+    >>> from pprint import pprint
     >>> triples = [Triple('T-76.4115', 'rdf:type', 'Course'),
     ...     Triple('T-76.4115', 'name', 'Software Development Project'),
     ...     Triple('T-76.4115', 'extent', '5-8')]
-    >>> expected = {
-    ...     uri('T-76.4115'): {
-    ...         uri('rdf:type'): literal('Course'),
-    ...         uri('name'): literal('Software Development Project'),
-    ...         uri('extent'): literal('5-8')
-    ...     }
-    ... }
-    >>> make_graph(triples) == expected
-    True
+    >>> pprint(make_graph(triples))
+    {'T-76.4115': {'extent': '5-8',
+                   'name': 'Software Development Project',
+                   'rdf:type': 'Course'}}
 
     When there are multiple triples with the same subject and predicate, the
     objects of the triples are put in a list:
@@ -31,22 +95,18 @@ def make_graph(triples):
     ...     'http://cos.alpha.sizl.org/people/ID#dRq9He3yWr3QUKaaWPEYjL',
     ...     'has_friend',
     ...     'http://cos.alpha.sizl.org/people/ID#azEe6yRdCr3OiIaaWPfx7J')]
-    >>> expected = {
-    ...     uri('http://cos.alpha.sizl.org/people/ID#dRq9He3yWr3QUKaaWPEYjL'): {
-    ...         uri('rdf:type'): literal('http://cos.alpha.sizl.org/people#Person'),
-    ...         uri('has_friend'): [
-    ...             literal('http://cos.alpha.sizl.org/people/ID#azAC7-RdCr3OiIaaWPfx7J'),
-    ...             literal('http://cos.alpha.sizl.org/people/ID#azEe6yRdCr3OiIaaWPfx7J')
-    ...         ]
-    ...     }
-    ... }
-    >>> make_graph(triples) == expected
-    True
+    >>> pprint(make_graph(triples))
+    {'http://cos.alpha.sizl.org/people/ID#dRq9He3yWr3QUKaaWPEYjL':
+        {'has_friend':
+            ['http://cos.alpha.sizl.org/people/ID#azAC7-RdCr3OiIaaWPfx7J',
+             'http://cos.alpha.sizl.org/people/ID#azEe6yRdCr3OiIaaWPfx7J'],
+         'rdf:type': 'http://cos.alpha.sizl.org/people#Person'}}
+
 
     """
     graph = {}
     for triple in triples:
-        s, p, o = triple.subject, triple.predicate, triple.object
+        s, p, o = str(triple.subject), str(triple.predicate), str(triple.object)
         if s not in graph:
             graph[s] = {}
         if p not in graph[s]:

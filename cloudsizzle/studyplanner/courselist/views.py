d3d1bc706db252a39a92efb0ef3443d63469729c
Ville Koivunen <vikoivun@gmail.com>
2010-02-04 15:39:54 +0000

    courselist/views.py: Courselist that works with the direct-to-smart-m3 API styles.css: CSS style forgotten from previous commit [147] login-register and base: templates for login page and generic base. Only slightly modified from Jari's work

--- cloudsizzle/studyplanner/courselist/views.py
+++ cloudsizzle/studyplanner/courselist/views.py
@@ -3,27 +3,34 @@ from django.shortcuts import render_to_response
 from studyplanner.courselist.models import Course
 from studyplanner.courselist.models import Faculty
 from studyplanner.courselist.models import Department
+from cloudsizzle.api.course import get_faculties, get_departments_by_faculty, get_courses_by_department, get_course
 
 def list_courses(request, faculty, department):
-    department = Department.objects.get(slug=department)
-    courses = department.courses.all()
-    return render_to_response('list_courses.html', {'user': request.user,
+    courses = get_courses_by_department(department)
+    department = {'slug': str(department)}
+    return render_to_response('courselist/list_courses.html', {'user': request.user,
         'department': department, 'courses': courses})
 
 def list_faculties(request):
-    faculties = Faculty.objects.all()
-    return render_to_response('list_faculties.html', {'user': request.user,
+#    faculties = Faculty.objects.all()
+    faculties = get_faculties()
+    print faculties
+    return render_to_response('courselist/list_faculties.html', {'user': request.user,
         'faculties': faculties})
 
 def list_departments(request, faculty):
-    faculty = Faculty.objects.get(slug=faculty)
-    departments = faculty.departments.all()
-    return render_to_response('list_departments.html', {'user': request.user,
+#    faculty = Faculty.objects.get(slug=faculty)
+#    departments = faculty.departments.all()
+
+    departments = get_departments_by_faculty(faculty)
+    faculty = {'slug': str(faculty)}
+    return render_to_response('courselist/list_departments.html', {'user': request.user,
         'faculty': faculty, 'departments': departments})
 
 def show_course(request, faculty, department, course):
-    faculty = Faculty.objects.get(slug=faculty)
-    department = Department.objects.get(slug=department)
-    course = Course.objects.get(slug=course)
-    return render_to_response('show_course.html', {'user': request.user,
+#    faculty = Faculty.objects.get(slug=faculty)
+#    department = Department.objects.get(slug=department)
+#    course = Course.objects.get(slug=course)
+    course = get_course(course)
+    return render_to_response('courselist/show_course.html', {'user': request.user,
         'faculty': faculty, 'department': department, 'course': course})

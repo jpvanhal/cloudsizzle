6afbcfbf6106767e428a89d98cd9d26b09c8f319
Ville Koivunen <vikoivun@gmail.com>
2010-02-06 15:44:18 +0000

    Updated views and URL definitions that work with the case-sensitive course API Ticket #294

--- cloudsizzle/studyplanner/courselist/urls.py
+++ cloudsizzle/studyplanner/courselist/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('courselist.views',
     (r'^$', 'list_faculties'),
     (r'^(?P<faculty>[a-z]+)$', 'list_departments'),
-    (r'^(?P<faculty>[a-z]+)/(?P<department>[a-z0-9-]+)$', 'list_courses'),
-    (r'^(?P<faculty>[a-z]+)/(?P<department>[a-z0-9-]+)/(?P<course>[a-z0-9-\.]+)$', 'show_course'),
+    (r'^(?P<faculty>[a-z]+)/(?P<department>[a-zA-Z0-9-]+)$', 'list_courses'),
+    (r'^(?P<faculty>[a-z]+)/(?P<department>[a-zA-Z0-9-]+)/(?P<course>[a-zA-Z0-9-\.]+)$', 'show_course'),
 )
--- cloudsizzle/studyplanner/courselist/views.py
+++ cloudsizzle/studyplanner/courselist/views.py
@@ -1,36 +1,40 @@
-# Create your views here.
+"""Views for browsing through available courses"""
 from django.shortcuts import render_to_response
 from studyplanner.courselist.models import Course
 from studyplanner.courselist.models import Faculty
 from studyplanner.courselist.models import Department
-from cloudsizzle.api.course import get_faculties, get_departments_by_faculty, get_courses_by_department, get_course
-
-def list_courses(request, faculty, department):
-    courses = get_courses_by_department(department)
-    department = {'slug': str(department)}
-    return render_to_response('courselist/list_courses.html', {'user': request.user,
-        'department': department, 'courses': courses})
+from cloudsizzle import api
 
 def list_faculties(request):
-#    faculties = Faculty.objects.all()
-    faculties = get_faculties()
-    print faculties
+    faculties = api.course.get_faculties()
     return render_to_response('courselist/list_faculties.html', {'user': request.user,
         'faculties': faculties})
 
 def list_departments(request, faculty):
-#    faculty = Faculty.objects.get(slug=faculty)
-#    departments = faculty.departments.all()
+    faculty = api.course.get_faculty_info(faculty)
+    print "faculty:"
+    print faculty
+    departments = api.course.get_departments_by_faculty(faculty['code'])
 
-    departments = get_departments_by_faculty(faculty)
-    faculty = {'slug': str(faculty)}
     return render_to_response('courselist/list_departments.html', {'user': request.user,
         'faculty': faculty, 'departments': departments})
 
+def list_courses(request, faculty, department):
+    # Faculty information is neeed for breadcrumbs
+    faculty = api.course.get_faculty_info(faculty)
+    department = api.course.get_department_info(department)
+    
+    courses = api.course.get_courses_by_department(department['code'])
+
+    return render_to_response('courselist/list_courses.html', {'user': request.user,
+        'faculty': faculty, 'department': department, 'courses': courses})
+
+
 def show_course(request, faculty, department, course):
-#    faculty = Faculty.objects.get(slug=faculty)
-#    department = Department.objects.get(slug=department)
-#    course = Course.objects.get(slug=course)
-    course = get_course(course)
+    faculty = api.course.get_faculty_info(faculty)
+    department = api.course.get_department_info(department)
+    
+    course = api.course.get_course(course)
+    
     return render_to_response('courselist/show_course.html', {'user': request.user,
         'faculty': faculty, 'department': department, 'course': course})

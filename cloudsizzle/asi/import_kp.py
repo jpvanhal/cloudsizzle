f33bd4f94ae98a756698e1cc66b9f71af9de82de
Janne Vanhala <janne.vanhala@gmail.com>
2010-02-07 13:53:29 +0000

    Fixed friend triples to use consistent uris.

--- cloudsizzle/asi/import_kp.py
+++ cloudsizzle/asi/import_kp.py
@@ -3,63 +3,54 @@ from asilib import ASIConnection
 # because we want to copy all users not synchronize a single user
 from asibsync.sib_agent import to_rdf_instance
 from cloudsizzle.kp import SIBConnection, Triple, bnode, uri, literal
-#from cloudsizzle import settings
+from cloudsizzle import settings
+
+
+# These are used to construct the URIs in RDF
+RDF_BASE_URI = 'http://cos.alpha.sizl.org/people'
+RDF_BASE_TYPE = 'Person'
+
+
+def friends_to_rdf(user, friends):
+    triples = []
+    for friend in friends:
+        triples.append(Triple(
+            '{0}/ID#{1}'.format(RDF_BASE_URI, user['id']),
+            '{0}#Friend'.format(RDF_BASE_URI),
+            uri('{0}/ID#{1}'.format(RDF_BASE_URI, friend['id']))
+        ))
+    return triples
 
 
 def import_asi():
-    # These are used to construct the URIs in RDF
-    RDF_BASE_URI = 'http://cos.alpha.sizl.org/people'
-    RDF_BASE_TYPE = 'Person'
+    with SIBConnection('ASI to SIB dump', method='preconfigured') as sc:
 
-    sc = SIBConnection('ASI to SIB dump', method='preconfigured')
-    
-    # Get ASI parameters from home directory, no wrapper doing this
-    import os
-    conf = {}
-    execfile(os.getenv('HOME', '.') + '/.asirc', conf)
-    if not ('asi_app_params' in conf):
-        print('asi_app_params not defined.')
-        sys.exit(1)
-    ac = ASIConnection(**conf['asi_app_params'])
-    ac.open()
-    
-    print "Fetching users from ASI"
-    users = ac.find_users()
-    # This will choke on users that do not have an id
-    # All users should probably have one, but we need to work
-    # nevertheless.
-    # Either need to convert the triplets one by one or patch
-    # asibsync
-    print "Converting ASI output to Triplets"
-    for index, user in enumerate(users):
-        print index, len(users), "\r"
-        try:
-            friends = ac.get_friends(user['id'])
-            user_triplet = to_rdf_instance(user, RDF_BASE_URI, RDF_BASE_TYPE, 'id')
-            for friend in friends:
-                # This probably ought to be converted to ASI URL format
-                user_triplet.append(
-                    Triple(
-                        "http://cloudsizzle.cs.hut.fi/onto/people/{0}".format(user['id']), 
-                        'has_friend', 
-                        "http://cloudsizzle.cs.hut.fi/onto/people/{0}".format(friend['id'])))
-        except KeyError:
-            print "Faulty userdata for ASI:"
-            print user
-        else:
-#            print "Inserting:"
-#            print user_triplet
-            print "Inserting the following triplet:"
-            print user_triplet
-            sc.insert(user_triplet)
-    
-    # This alone is not enough, old triplets need to be removed first
-    # Perhaps add a 'data owner' triplets identifying all subjects
-    # that can be removed? Or just query for all triplets, find ones
-    # containing RDF_BASE_URI and remove them
-#    print "Inserting Triplets to SIB"
-#    sc.insert(user_triplets)
+        with ASIConnection(base_url=settings.ASI_BASE_URL,
+            app_name=settings.ASI_APP_NAME,
+            app_password=settings.ASI_APP_PASSWORD) as ac:
+
+            print "Fetching users from ASI"
+            users = ac.find_users()
+            # This will choke on users that do not have an id
+            # All users should probably have one, but we need to work
+            # nevertheless.
+            # Either need to convert the triplets one by one or patch
+            # asibsync
+            print "Converting ASI output to Triplets"
+            for index, user in enumerate(users):
+                print index, len(users), "\r"
+                try:
+                    triples = to_rdf_instance(
+                        user, RDF_BASE_URI, RDF_BASE_TYPE, 'id')
+                    friends = ac.get_friends(user['id'])
+                    triples.extend(friends_to_rdf(user, friends))
+                except KeyError:
+                    print "Faulty userdata for ASI:"
+                    print user
+                else:
+                    print "Inserting the following triples:"
+                    print triples
+                    sc.insert(triples)
 
 if __name__ == '__main__':
     import_asi()
-    

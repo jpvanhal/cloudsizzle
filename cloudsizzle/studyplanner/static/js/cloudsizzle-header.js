9f4130d7dfa162b28329217d773d688d5e9025dc
Kristoffer Snabb <kristoffer.snabb@tkk.fi>
2010-02-14 13:18:03 +0000

    friend request ajax function made better so that the error function will be visible if everything did not go correctly. also shows processing when request is beeing processed.

--- cloudsizzle/studyplanner/static/js/cloudsizzle-header.js
+++ cloudsizzle/studyplanner/static/js/cloudsizzle-header.js
@@ -1,5 +1,16 @@
-function addfriend(url, node) {
-    $.post(url);
-    document.getElementById(node).innerHTML = "friend request sent";
-    document.getElementById(node).onclick = "";
+function addfriend(uri, node) {
+    $.ajax({
+        url: uri,
+        type: "POST",
+        beforeSend: function(XMLHttpRequest) {
+            document.getElementById(node).innerHTML = "processing";
+            document.getElementById(node).onclick = "";
+        },
+        success: function(data, textStatus, XMLHttpRequest) {
+            document.getElementById(node).innerHTML = "friend request sent";
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            document.getElementById(node).innerHTML = textStatus;
+        }
+    });
 }

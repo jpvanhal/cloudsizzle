743dc3c25a012789bdc570adde26c1acd3913730
Janne Vanhala <janne.vanhala@gmail.com>
2010-02-14 19:43:19 +0000

    Fixed #353

--- cloudsizzle/scrapers/oodi/pipelines.py
+++ cloudsizzle/scrapers/oodi/pipelines.py
@@ -4,15 +4,15 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
-from cloudsizzle.scrapers.items import FacultyItem, DepartmentItem, CourseItem, CourseOverviewItem, CompletedCourseItem, ModuleItem
+from cloudsizzle.scrapers.items import CompletedCourseItem, ModuleItem
 from cloudsizzle.kp import SIBConnection, Triple, literal, uri, bnode
 from scrapy.conf import settings
 
 class SIBPipeline(object):
     def __init__(self):
-        self.asi_username = settings['ASI_USERNAME']
-        if not self.asi_username:
-            self.asi_username = raw_input('ASI Username: ')
+        self.asi_user_id = settings['ASI_USER_ID']
+        if not self.asi_user_id:
+            self.asi_user_id = raw_input('ASI User ID: ')
 
         self.sc = SIBConnection(method='preconfigured')
         self.sc.open()
@@ -22,22 +22,24 @@ class SIBPipeline(object):
 
     def transform_to_triples(self, item):
         if isinstance(item, CompletedCourseItem):
+            namespace = 'http://cloudsizzle.cs.hut.fi/ontology/'
+            uri = '{0}people/{1}/courses/completed/{2}'.format(
+                namespace, self.asi_user_id, item['code'])
             return [
-                Triple(self.asi_username, 'rdf:type', 'Person'),
-                Triple(self.asi_username, 'has_completed', bnode('id')),
-                Triple(bnode('id'), 'rdf:type', 'CompletedCourse'),
-                Triple(bnode('id'), 'code', item['code']),
-                Triple(bnode('id'), 'name', item['name']),
-                Triple(bnode('id'), 'cr', item['cr']),
-                Triple(bnode('id'), 'ocr', item['ocr']),
-                Triple(bnode('id'), 'grade', item['grade']),
-                Triple(bnode('id'), 'date', item['date'].isoformat()),
-                Triple(bnode('id'), 'teacher', item['teacher'])]
-                # TODO: module is missing
-        elif isinstance(item,ModuleItem):
-            return [
-                Triple(item['code'], 'rdf:type', 'Module'),
-                Triple(item['code'], 'name', item['name'])]
+                Triple(uri, 'rdf:type', 'CompletedCourse'),
+                Triple(uri, 'user_id', self.asi_user_id),
+                Triple(uri, 'code', item['code']),
+                Triple(uri, 'name', item['name']),
+                Triple(uri, 'cr', item['cr']),
+                Triple(uri, 'ocr', item['ocr']),
+                Triple(uri, 'grade', item['grade']),
+                Triple(uri, 'date', item['date'].isoformat()),
+                Triple(uri, 'teacher', item['teacher'])]
+        # TODO: module is missing
+        # elif isinstance(item,ModuleItem):
+        #    return [
+        #        Triple(item['code'], 'rdf:type', 'Module'),
+        #        Triple(item['code'], 'name', item['name'])]
 
     def process_item(self, spider, item):
         triples = [triple for triple in self.transform_to_triples(item) if triple.object]

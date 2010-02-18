f909366494d588c19fa9fa8a03da12b94929e2a1
Janne Vanhala <janne.vanhala@gmail.com>
2010-02-18 14:05:48 +0000

    Fixed #405. References #408.

--- cloudsizzle/studyplanner/common/forms.py
+++ cloudsizzle/studyplanner/common/forms.py
@@ -1,21 +1,80 @@
-""" Common form. Currently these are not so common, perhaps move them to login page"""
+"""
+Common forms.
+
+Currently these are not so common, perhaps move them to login page
+
+"""
 from django import forms
 # For custom form validation (password verification)
 from django.forms.util import ErrorList
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(min_length=4, max_length=20)
     password = forms.CharField(min_length=4, widget=forms.PasswordInput)
 
+
 class RegisterForm(forms.Form):
     # Required fields have * at the end of label
-    username = forms.CharField(label="Username*", min_length=4, max_length=20, error_messages={'required':'Please enter a username', 'min_length':'The username must be at least 4 characters', 'max_length':'The username must be less than 21 characters'})
-    firstname = forms.CharField(label="First name", max_length=40, required=False, error_messages={'max_length':'First name must be less than 41 characters'})
-    lastname = forms.CharField(label="Last name", max_length=40, required=False, error_messages={'max_length':'Last name must be less than 41 characters'})
-    password = forms.CharField(label="Password*", min_length=4, widget=forms.PasswordInput, error_messages={'required':'Please enter a password', 'min_length':'Password must be at least 4 characters'})
-    password_conf = forms.CharField(label="Confirm password*", min_length=4, widget=forms.PasswordInput, error_messages={'required':'Please confirm your password', 'min_length':'Password must be at least 4 characters'})
-    email = forms.EmailField(label="E-mail*", max_length=60, error_messages={'required':'Please enter your e-mail', 'max_length':'E-mail must be less than 61 characters'})
-    consent = forms.BooleanField(label="I give my consent to the research study*", required=True, error_messages={'required':'You must give your consent to research study in order to register'})
+    username = forms.CharField(
+        label="Username*",
+        min_length=4,
+        max_length=20,
+        error_messages={
+            'required': 'Please enter a username',
+            'min_length': 'The username must be at least 4 characters',
+            'max_length': 'The username must be less than 21 characters'
+        }
+    )
+    firstname = forms.CharField(
+        label="First name",
+        max_length=40,
+        required=False,
+        error_messages={
+            'max_length': 'First name must be less than 41 characters'
+        }
+    )
+    lastname = forms.CharField(
+        label="Last name",
+        max_length=40,
+        required=False,
+        error_messages={
+            'max_length': 'Last name must be less than 41 characters'
+        })
+    password = forms.CharField(
+        label="Password*",
+        min_length=4,
+        widget=forms.PasswordInput,
+        error_messages={
+            'required': 'Please enter a password',
+            'min_length': 'Password must be at least 4 characters'
+        }
+    )
+    password_conf = forms.CharField(
+        label="Confirm password*",
+        min_length=4,
+        widget=forms.PasswordInput,
+        error_messages={
+            'required': 'Please confirm your password',
+            'min_length': 'Password must be at least 4 characters'
+        }
+    )
+    email = forms.EmailField(
+        label="E-mail*",
+        max_length=60,
+        error_messages={
+            'required': 'Please enter your e-mail',
+            'max_length': 'E-mail must be less than 61 characters'
+        }
+    )
+    consent = forms.BooleanField(
+        label="I give my consent to the research study*",
+        required=True,
+        error_messages={
+            'required': 'You must give your consent to research study in '
+                        'order to register'
+        }
+    )
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -31,7 +90,7 @@ class RegisterForm(forms.Form):
             # errors near their associated fields you might
             # want to uncomment this.
             #self._errors["password_conf"] = ErrorList([msg])
-            
+
             # Django example also removed the fields from cleaned
             # data. I'm not sure whether it is relevant here.
             del cleaned_data["password"]
--- cloudsizzle/studyplanner/common/planner_session.py
+++ cloudsizzle/studyplanner/common/planner_session.py
@@ -1,46 +1,39 @@
-"""Classes/functions related to global session management in the studyplanner"""
+"""
+Classes/functions related to global session management in the studyplanner.
+
+"""
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from api import Session
 
-"""Decorator that checks if the user is authenticated. If not, they are
-returned the login page.
 
-"""
-class check_authentication(object):
-    def __init__(self, func):
-        self.func = func
+def check_authentication(func):
+    """Decorator that checks if the user is authenticated. If not, they are
+    returned the login page.
 
-    def __call__(self, request, *args, **kwargs):
-        if 'asi_session' in request.session:
-            return self.func(request, *args, **kwargs)
+    """
+    def wrapped(request, *args, **kwargs):
+        if is_authenticated(request):
+            return func(request, *args, **kwargs)
         else:
-            return HttpResponseRedirect(reverse(frontpage))
+            return HttpResponseRedirect(reverse('frontpage'))
+    return wrapped
 
-""" Dis is a decorator
 
-@check_authentication
-def foo():
-  pass
-
-foo = check_authentication(foo)
-"""
-
-"""Checks if the user is authenticated. Returns True/False"""
 def is_authenticated(request):
-    if 'asi_session' in request.session:
-        return True
-    else:
-        return False
+    """Checks if the user is authenticated. Returns True/False"""
+    return 'asi_session' in request.session
 
-"""Tries to authenticate user.
 
-Status of authentication can be checked with is_authenticated()
-Will also pass through LoginFailed exception from api
-
-This is still a dummy that will authenticate anyone with any password
-
-"""
 def authenticate(request, username, password):
+    """Tries to authenticate user.
+
+    Status of authentication can be checked with is_authenticated()
+    Will also pass through LoginFailed exception from api
+
+    This is still a dummy that will authenticate anyone with any password
+
+    """
     # Call the ASI authentication functions here
     asi_session = Session(username, password)
     # This will throw exception for invalid username
